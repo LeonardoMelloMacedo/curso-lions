328430951518643972767675b9057e83ad1a908b
leonardo mello macedo <leonardomellomacedo@gmail.com>
2026-03-25 17:03:37 -0300

    ex 09 da lista de vetores

--- lista-vetor-01/ex01.js
+++ lista-vetor-01/ex01.js
@@ -1,5 +1,12 @@
+const { i } = require("./ex09");
+
 let nums = [2, 8, 4, 9, 10, 3];
 
 for (let i = 0; i < nums.length; i++) {
     console.log(nums[i]);
+};
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i]) {
+        s++;
+    };
 }
