e5f7144dac2198beef91e8178754b2af38059c94
leonardo mello macedo <leonardomellomacedo@gmail.com>
2026-03-25 15:23:30 -0300

    ex 06 da lista de vetores

--- lista-vetor-01/ex06.js
+++ lista-vetor-01/ex06.js
@@ -1,9 +1,8 @@
-let nums = [1,2,3,4,5,6];
+let numbs = [1,2,3,4,5,6];
 let menor = numbs[0];
-for (let i = 0; i < nums.length; i++) {
-    if (numbs[i] <
-         menor){
-        menor = nums[i]
+for (let i = 0; i < numbs.length; i++) {
+    if (numbs[i] < menor){
+        menor = numbs[i]
 }
 }
 console.log(menor)
