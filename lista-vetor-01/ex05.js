9e631d2a92187961bcf7fb215806faefd4bafdd5
leonardo mello macedo <leonardomellomacedo@gmail.com>
2026-03-25 15:21:54 -0300

    ex 05 da lista de vetores

--- lista-vetor-01/ex05.js
+++ lista-vetor-01/ex05.js
@@ -1,8 +1,8 @@
-let nums = [1,2,3,4,5,6];
+let numbs = [1,2,3,4,5,6];
 let maior = numbs[0];
-for (let i = 0; i < nums.length; i++) {
+for (let i = 0; i < numbs.length; i++) {
     if (numbs[i] > maior){
-        maior = nums[1]
+        maior = numbs[i]
 }
 }
 console.log(maior)
---
+++ lista-vetor-01/ex06.js
@@ -0,0 +1,9 @@
+let nums = [1,2,3,4,5,6];
+let menor = numbs[0];
+for (let i = 0; i < nums.length; i++) {
+    if (numbs[i] <
+         menor){
+        menor = nums[i]
+}
+}
+console.log(menor)
