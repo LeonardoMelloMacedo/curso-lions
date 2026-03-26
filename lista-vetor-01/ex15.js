5f81dcc4b6095b359e42851f6bfc937d260837c5
leonardo mello macedo <leonardomellomacedo@gmail.com>
2026-03-26 15:49:50 -0300

    ex 15 da lista de vetores

--- lista-vetor-01/ex15.js
+++ lista-vetor-01/ex15.js
@@ -1,7 +1,14 @@
-let a = [1, 2, 3, 4, 5, 6]
-let b = []
+let a = [1, 2, 3, 4, 5, 6, 7, 8]
+let crescente = true;
 
 for (let i = 0; i < a.length; i++) {
-    b[i] = a[i]
+    if([i] > a[i +1]) {
+        crescente = false;
+    }
 }
-console.log(b);
+
+if (crescente) {
+    console.log("crescente")
+} else {
+    console.log("nao crescente")
+}
